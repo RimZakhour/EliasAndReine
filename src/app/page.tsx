6c32c52cc8c860c6e6b392f7b2dcc85b00aa3a8a
RimZakhour <33280045+RimZakhour@users.noreply.github.com>
2025-04-16 15:46:58 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -481,8 +481,7 @@ if (submitted) {
       <button
   type="button"
   onClick={addGuest}
-  className="inline-flex w-[100px] h-[25px] text-center items-center gap-2 border-2 border-black text-black rounded-md px-4 py-2 bg-white hover:bg-black hover:text-white transition"
->
+  className="inline-flex items-center justify-center gap-2 w-[140px] h-[50px] bg-white border-2 border-black text-black text-sm font-medium hover:bg-black hover:text-white transition focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 shadow-md">
     + Add Person
 </button>
       </div>
@@ -504,7 +503,7 @@ if (submitted) {
       <button 
   type="submit"
   disabled={loading}
-  className={`w-full h-[50px] border-2 border-black bg-white text-black text-lg font-serif hover:bg-black hover:text-white transition shadow-lg disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none focus:ring-2 focus:ring-black ${
+  className={`w-full h-[50px] bg-white border-2 border-black text-black text-lg font-serif hover:bg-black hover:text-white transition shadow-lg disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 ${
     loading ? 'bg-gray-200 cursor-wait' : ''
   }`}
 >
